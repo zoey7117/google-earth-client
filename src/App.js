3451d3165f00e88040019dcc967c8caefb6b6937
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-04-21 20:46:36 -0400

    changed home to search url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-import Opener from './components/Opener';
+import React from 'react'
+import './App.css'
+import Home from './components/Home'
+import Opener from './components/Opener'
 
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Route exact path="/" component={Opener} />
-					<Route path="/home" component={Home} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Opener} />
+          <Route path="/search" component={Home} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Opener.js
+++ src/components/Opener.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from 'react'
 
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 class Opener extends React.Component {
-	render() {
-		return (
-			<div>
-				<div className="opener">
-					<div className="opener-content">
-						<h1>Earth Paints</h1>
-						<p>
-							<span>You're looking at Norway</span>, thanks to the Chrome Google Earth Extension. I've
-							screenshot many of the images because of their beauty. It amazes me that the earth from afar
-							resembles what artists emotionally strive to emulate. I made a search bar for countries with
-							the caveat that they aren't all represented. I've chosen images that mirror paintings imo.
-						</p>
-						<hr />
-						<Link to="/home" className="button">
-							enjoy!
-						</Link>
-					</div>
-					<img src="https://imgur.com/qI8C69i.png" className="image-opener" />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div className="opener">
+          <div className="opener-content">
+            <h1>Earth Paints</h1>
+            <p>
+              <span>You're looking at Norway</span>, thanks to the Chrome Google
+              Earth Extension. I've screenshot many of the images because of
+              their beauty. It amazes me that the earth from afar resembles what
+              artists emotionally strive to emulate. I made a search bar for
+              countries with the caveat that they aren't all represented. I've
+              chosen images that mirror paintings imo.
+            </p>
+            <hr />
+            <Link to="/search" className="button">
+              enjoy!
+            </Link>
+          </div>
+          <img src="https://imgur.com/qI8C69i.png" className="image-opener" />
+        </div>
+      </div>
+    )
+  }
 }
-export default Opener;
+export default Opener
