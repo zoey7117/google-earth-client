11ee802b5c33df0ef7eff7631ebc6d2cb9c6858c
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-04-21 20:38:50 -0400

    cleanup

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
-
-import { Card, Image } from 'semantic-ui-react';
+import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
+import { Card } from 'semantic-ui-react'
 
 const Images = (props) => {
-	return (
-		<Card key={props.id} className="photo" onClick={() => props.handleCardView(props.image)}>
-			<LazyLoadImage
-				src={props.image.image}
-				alt=""
-				placeholderSrc={<span />}
-				effect="blur"
-				data-id={props.id}
-				className="photo-image"
-				width={265}
-				height={265}
-			/>
-		</Card>
-		// <Image src={props.image.image} alt="" data-id={props.id} className="photo-image" height={265} />
-		// </Card>
-	);
-};
-export default Images;
+  return (
+    <Card
+      key={props.id}
+      className="photo"
+      onClick={() => props.handleCardView(props.image)}
+    >
+      <LazyLoadImage
+        src={props.image.image}
+        alt=""
+        placeholderSrc={<span />}
+        effect="blur"
+        data-id={props.id}
+        className="photo-image"
+        width={265}
+        height={265}
+      />
+    </Card>
+  )
+}
+export default Images
